dc1513dba37f39b3a96cf450f1c0535ff2df0952
gharbi-collab <mohamedhedigharbi101@gmail.com>
2023-11-13 17:04:56 +0100

    list

---
+++ grandeurs/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('grandeurs/<int:id>/', views.detail, name='detail'),
+    path('grandeurs', views.grandeur_list, name="grandeurs")  
+]
--- grandeurs/views.py
+++ grandeurs/views.py
@@ -2,13 +2,9 @@ from django.shortcuts import render
 from .models import Grandeur
 
 
-# Create your views here.
-
 def detail(request, id):
     grandeur = Grandeur.objects.get(pk=id)
-    return render(request, "grandeurs/detail.html", {"grandeur": grandeur})
+    return render(request, "grandeurs/detail.html", {"grandeur": grandeur}) 
 
-
-def grnadeur_list(request):
-    return render(request, "grandeurs/grandeur_list.html", {"grandeurs", Grandeur.objects.all() })
-
+def grandeur_list(request):  # Correction du nom de la fonction
+    return render(request, "grandeurs/grandeur_list.html", {"grandeurs": Grandeur.objects.all() })  
